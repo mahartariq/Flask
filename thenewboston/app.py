eb73c2a7b97de61bbb3adeeb32e63f7e89351478
Muhammad Tariq <mahartariq499@gmail.com>
2021-02-28 20:12:51 +0500

    routing

--- thenewboston/app.py
+++ thenewboston/app.py
@@ -3,10 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "This is the homepage"
+def Hello():
+    return 'Hello World!'
 
+@app.route('/home')
+def Home():
+    return '<div> <h1>Deep Learning Model</h1><br/><h2>Cat vs Dog</h2> </div>'
+
+@app.route('/profile/<username>')
+def profile(username):
+    return "<h2>Hi! %s</h2>" % username    
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
